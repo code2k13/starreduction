6340ab990641ee77c56da18e4869847c92440da4
code2k13 <ashpat.api@gmail.com>
2021-10-15 12:39:36 +0000

    Fixed progress bar issues

--- removestars.py
+++ removestars.py
@@ -12,12 +12,14 @@ import random
 import numpy as np
 import sys
 from tqdm import tqdm
+import math
 
 IMG_SIZE = 1024
 all_outputs = 1
 pad_width = 24
 total_steps = 0
 progress_bar = None
+current_progress= 0
 
 args = sys.argv 
 if len(args) != 3:
@@ -27,7 +29,7 @@ if len(args) != 3:
 
 
 def process_channel(channel,pad_width,input_image_size):
-    global progress_bar,step_size
+    global progress_bar,step_size,current_progress
     output_image =  Image.new('L', input_image_size)   
     for i in range(0,int(channel.size[0]/IMG_SIZE)):
         for j in range(0,int(channel.size[1]/IMG_SIZE)):         
@@ -44,7 +46,8 @@ def process_channel(channel,pad_width,input_image_size):
             predicted_section = predicted_section.crop((pad_width,pad_width,IMG_SIZE-pad_width,IMG_SIZE-pad_width))
             predicted_section = predicted_section.resize((IMG_SIZE,IMG_SIZE))   
             output_image.paste(predicted_section, (i*IMG_SIZE,j*IMG_SIZE),  mask=None)          
-            progress_bar.update(step_size)       
+            progress_bar.update(step_size) 
+            current_progress = current_progress + step_size      
     return output_image
 
 G2 = model.Generator()
@@ -56,26 +59,22 @@ size = source_image.size
 max_dimension = max(size)
 
 a,b = divmod(max_dimension,1024)
-if b !=0 :
-  max_dimension = max_dimension + b
+if b > 0 :
+  max_dimension = (a+1)*1024
  
 input_image = source_image.crop((0,0,max_dimension,max_dimension))
-
+progress_bar = tqdm(total=100)
 
 if mode == 'L':  
-  total_steps = (max_dimension/1024)**2 
-  step_size = int((1/total_steps)*100)
-  progress_bar = tqdm(total=100)
-  progress_bar.update(step_size)
+  total_steps = math.ceil((max_dimension/1024)**2) 
+  step_size = math.ceil((1/total_steps)*100)  
   output = process_channel(input_image,pad_width,input_image.size)
   output = output.crop((0,0,size[0],size[1]))
   output.save(args[2])
-  progress_bar.update(step_size)
+  progress_bar.update(100-current_progress)
 elif mode == 'RGB' or mode == 'RGBA':
-  total_steps = 3*(max_dimension/1024)**2
+  total_steps = int(3*(max_dimension/1024)**2)
   step_size = int((1/total_steps)*100)  
-  progress_bar = tqdm(total=100)
-  progress_bar.update(step_size)
   channels = Image.Image.split(input_image)
   all_outputs  = []
   for channel in channels[0:3]:
@@ -84,7 +83,7 @@ elif mode == 'RGB' or mode == 'RGBA':
     all_outputs.append(channel_output) 
   output =Image.merge('RGB', (all_outputs[0],all_outputs[1],all_outputs[2]))
   output.save(args[2])
-  progress_bar.update(step_size)  
+  progress_bar.update(100-current_progress)  
 else:
   print("Invalid mode for input image:", mode)
   print("Only grayscale(L) and RGB(RGBA) images are supported.")
