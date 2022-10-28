5c5791a377eb977b1a97a365454bb3fe8c7d6dd1
code2k13 <ashpat.api@gmail.com>
2022-10-28 13:28:19 +0000

    added new model and weights

--- model.py
+++ model.py
@@ -3,74 +3,58 @@ import tensorflow as tf
 IMG_SIZE = 1024
 OUTPUT_CHANNELS = 1
 
-def downsample(filters, size, apply_batchnorm=True):
+def downsample(filters, size, apply_batchnorm=True,strides = 2,name=''):
   initializer = tf.random_normal_initializer(0., 0.02)
-
-  result = tf.keras.Sequential()
-  result.add(
-      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
-                             kernel_initializer=initializer, use_bias=False))
+  result = tf.keras.Sequential(name=name)
+  result.add(tf.keras.layers.Conv2D(filters, size, strides=strides, padding='same',kernel_initializer=initializer, use_bias=False))
 
   if apply_batchnorm:
     result.add(tf.keras.layers.BatchNormalization())
 
   result.add(tf.keras.layers.LeakyReLU())
-
   return result
 
-def upsample(filters, size, apply_dropout=False):
+def upsample(filters, size, apply_dropout=False,strides = 2,name=''):
   initializer = tf.random_normal_initializer(0., 0.02)
-
-  result = tf.keras.Sequential()
-  result.add(
-    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
-                                    padding='same',
-                                    kernel_initializer=initializer,
-                                    use_bias=False))
+  result = tf.keras.Sequential(name=name)
+  result.add(tf.keras.layers.Conv2DTranspose(filters, size, strides=strides,padding='same',
+                                    kernel_initializer=initializer,use_bias=False))
 
   result.add(tf.keras.layers.BatchNormalization())
-
   if apply_dropout:
-      result.add(tf.keras.layers.Dropout(0.5))
-        
+      result.add(tf.keras.layers.Dropout(0.5))        
   result.add(tf.keras.layers.ReLU())
-
   return result
 
 def Generator():
-    
   inputs = tf.keras.layers.Input(shape=[IMG_SIZE, IMG_SIZE, OUTPUT_CHANNELS])
 
   down_stack = [
-    downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
-    downsample(128, 4),  # (batch_size, 64, 64, 128)
-    downsample(256, 4),  # (batch_size, 32, 32, 256)
-    downsample(512, 4),  # (batch_size, 16, 16, 512)
-    downsample(512, 4),  # (batch_size, 8, 8, 512)
-    downsample(512, 4),  # (batch_size, 4, 4, 512)
-    downsample(512, 4),  # (batch_size, 2, 2, 512)
-    downsample(512, 4),  # (batch_size, 1, 1, 512)
+    downsample(16, 5, apply_batchnorm=False,strides = 1, name='gd_1'),  # (batch_size, 128, 128, 64)
+    downsample(32, 5,name='gd_2'),  # (batch_size, 64, 64, 128)
+    downsample(64, 5,name='gd_3'),  # (batch_size, 32, 32, 256)
+    downsample(128, 5,name='gd_4'),  # (batch_size, 16, 16, 512)
+    downsample(256, 5,name='gd_5'),  # (batch_size, 8, 8, 512)
+    downsample(256, 5,name='gd_6'),  # (batch_size, 4, 4, 512)
+    downsample(512, 5,name='gd_7'),  # (batch_size, 2, 2, 512)
+    downsample(512, 5,name='gd_8'),  # (batch_size, 1, 1, 512)
   ]
 
   up_stack = [
-    upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
-    upsample(512, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
-    upsample(512, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-    upsample(512, 4),  # (batch_size, 16, 16, 1024)
-    upsample(256, 4),  # (batch_size, 32, 32, 512)
-    upsample(128, 4),  # (batch_size, 64, 64, 256)
-    upsample(64, 4),  # (batch_size, 128, 128, 128)
+    upsample(512, 5, apply_dropout=True,name='gu_1'),  # (batch_size, 2, 2, 1024)
+    upsample(256, 5, apply_dropout=True,name='gu_2'),  # (batch_size, 4, 4, 1024)
+    upsample(256, 5, apply_dropout=True,name='gu_3'),  # (batch_size, 8, 8, 1024)      
+    upsample(128, 5,name='gu_4'),  # (batch_size, 16, 16, 1024)
+    upsample(64, 5,name='gu_5'),  # (batch_size, 32, 32, 512)
+    upsample(32, 5,name='gu_6'),  # (batch_size, 64, 64, 256)
+    upsample(16, 5,name='gu_7'),  # (batch_size, 128, 128, 128)
   ]
 
   initializer = tf.random_normal_initializer(0., 0.02)
-  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
-                                         strides=2,
-                                         padding='same',
-                                         kernel_initializer=initializer,
-                                         activation='tanh')  # (batch_size, 256, 256, 3)
+  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,strides=1,padding='same',
+                                         kernel_initializer=initializer,activation='relu')  # (batch_size, 256, 256, 3)
 
   x = inputs
-
   # Downsampling through the model
   skips = []
   for down in down_stack:
@@ -79,13 +63,10 @@ def Generator():
 
   skips = reversed(skips[:-1])
 
-  # Upsampling and establishing the skip connections
   for up, skip in zip(up_stack, skips):
-    #print(up_stack,skips)
     x = up(x)
     x = tf.keras.layers.Concatenate()([x, skip])
 
   x = last(x)
 
   return tf.keras.Model(inputs=inputs, outputs=x)
-
--- removestars.py
+++ removestars.py
@@ -6,17 +6,17 @@ import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import model
-import time
-from PIL import Image, ImageFilter,ImageEnhance
-import random 
+from PIL import Image
 import numpy as np
 import sys
 from tqdm import tqdm
 import math
 
-IMG_SIZE = 1024
+
+IMG_SIZE = 512
+MODEL_SIZE = 1024
 all_outputs = 1
-pad_width = 24
+pad_width = 2
 total_steps = 0
 progress_bar = None
 current_progress= 0
@@ -27,63 +27,69 @@ if len(args) != 3:
   print ('Argument List:', str(sys.argv)) 
   exit(1)
 
+def process_tile(channel,i,j,pad_width,model,output_image):
+  corp_rect = (i*IMG_SIZE,j*IMG_SIZE,i*IMG_SIZE+IMG_SIZE,j*IMG_SIZE+IMG_SIZE)
+  current_tile = channel.crop(corp_rect)
+  current_tile = current_tile.convert('L')
+  blank_image =  current_tile.copy()  
+  current_tile = current_tile.resize((IMG_SIZE-pad_width*2,IMG_SIZE-pad_width*2))
+  blank_image.paste(current_tile,(pad_width,pad_width))
+  blank_image = blank_image.resize((MODEL_SIZE,MODEL_SIZE))
+  blank_image  = np.asarray(blank_image,dtype="float16").reshape(1,MODEL_SIZE,MODEL_SIZE)/512
+  predicted_section = model.predict(blank_image,verbose=0)      
+  predicted_section = predicted_section.reshape(MODEL_SIZE,MODEL_SIZE)*1024
+  predicted_section = Image.fromarray(predicted_section).convert('L')
+  predicted_section =  predicted_section.resize((IMG_SIZE,IMG_SIZE))            
+  predicted_section = predicted_section.crop((pad_width,pad_width,IMG_SIZE-pad_width,IMG_SIZE-pad_width))
+  predicted_section = predicted_section.resize((IMG_SIZE,IMG_SIZE))  
+  output_image.paste(predicted_section, (i*IMG_SIZE,j*IMG_SIZE),  mask=None)
 
-def process_channel(channel,pad_width,input_image_size):
+def process_channel(channel,pad_width,input_image_size):    
     global progress_bar,step_size,current_progress
-    output_image =  Image.new('L', input_image_size)   
+    output_image =  Image.new('L', input_image_size)
     for i in range(0,int(channel.size[0]/IMG_SIZE)):
-        for j in range(0,int(channel.size[1]/IMG_SIZE)):         
-            corp_rect = (i*IMG_SIZE,j*IMG_SIZE,i*IMG_SIZE+IMG_SIZE,j*IMG_SIZE+IMG_SIZE)
-            current_tile = channel.crop(corp_rect)
-            current_tile = current_tile.convert('L')
-            blank_image =  current_tile.copy()  
-            current_tile = current_tile.resize((IMG_SIZE-pad_width*2,IMG_SIZE-pad_width*2))
-            blank_image.paste(current_tile,(pad_width,pad_width))
-            blank_image  = np.asarray(blank_image,dtype="float16").reshape(1,IMG_SIZE,IMG_SIZE)/255
-            predicted_section = G2.predict(blank_image,verbose=0)            
-            predicted_section = predicted_section.reshape(IMG_SIZE,IMG_SIZE)*255 
-            predicted_section = Image.fromarray(predicted_section).convert('L')            
-            predicted_section = predicted_section.crop((pad_width,pad_width,IMG_SIZE-pad_width,IMG_SIZE-pad_width))
-            predicted_section = predicted_section.resize((IMG_SIZE,IMG_SIZE))   
-            output_image.paste(predicted_section, (i*IMG_SIZE,j*IMG_SIZE),  mask=None)          
-            progress_bar.update(step_size) 
-            current_progress = current_progress + step_size      
+        for j in range(0,int(channel.size[1]/IMG_SIZE)):                   
+            process_tile(channel,i,j,pad_width,G2,output_image)
+            current_progress = current_progress + step_size
+            if current_progress <= 100:
+                progress_bar.update(round(step_size,2))
     return output_image
 
-G2 = model.Generator()
-G2.load_weights("weights/weights")
+try:
+  G2 = model.Generator()
+  G2.load_weights("weights/weights")
 
-source_image = Image.open(args[1])
-mode = source_image.mode
-size = source_image.size
-max_dimension = max(size)
+  source_image = Image.open(args[1])
+  mode = source_image.mode
+  size = source_image.size
+  max_dimension = max(size)
 
-a,b = divmod(max_dimension,1024)
-if b > 0 :
-  max_dimension = (a+1)*1024
- 
-input_image = source_image.crop((0,0,max_dimension,max_dimension))
-progress_bar = tqdm(total=100)
+  a,b = divmod(max_dimension,IMG_SIZE)
+  if b > 0 :
+    max_dimension = (a+1)*IMG_SIZE
+  
+  input_image = source_image.crop((0,0,max_dimension,max_dimension))
+  progress_bar = tqdm(total=100,leave=True)
 
-if mode == 'L':  
-  total_steps = math.ceil((max_dimension/1024)**2) 
-  step_size = math.ceil((1/total_steps)*100)  
-  output = process_channel(input_image,pad_width,input_image.size)
-  output = output.crop((0,0,size[0],size[1]))
-  output.save(args[2])
-  progress_bar.update(100-current_progress)
-elif mode == 'RGB' or mode == 'RGBA':
-  total_steps = int(3*(max_dimension/1024)**2)
-  step_size = int((1/total_steps)*100)  
-  channels = Image.Image.split(input_image)
-  all_outputs  = []
-  for channel in channels[0:3]:
-    channel_output = process_channel(channel,pad_width,input_image.size)
-    channel_output = channel_output.crop((0,0,size[0],size[1]))
-    all_outputs.append(channel_output) 
-  output =Image.merge('RGB', (all_outputs[0],all_outputs[1],all_outputs[2]))
-  output.save(args[2])
-  progress_bar.update(100-current_progress)  
-else:
-  print("Invalid mode for input image:", mode)
-  print("Only grayscale(L) and RGB(RGBA) images are supported.")
+  if mode == 'L':  
+    total_steps = math.ceil((max_dimension/IMG_SIZE)**2) 
+    step_size = (1/total_steps)*100
+    output = process_channel(input_image,pad_width,input_image.size)
+    output = output.crop((0,0,size[0],size[1]))
+    output.save(args[2])
+  elif mode == 'RGB' or mode == 'RGBA':
+    total_steps = int(3*(max_dimension/IMG_SIZE)**2)
+    step_size = (1/total_steps)*100 
+    channels = Image.Image.split(input_image)
+    all_outputs  = []
+    for channel in channels[0:3]:
+      channel_output = process_channel(channel,pad_width,input_image.size)
+      channel_output = channel_output.crop((0,0,size[0],size[1]))
+      all_outputs.append(channel_output) 
+    output =Image.merge('RGB', (all_outputs[0],all_outputs[1],all_outputs[2]))
+    output.save(args[2])
+  else:
+    print("Invalid mode for input image:", mode)
+    print("Only grayscale(L) and RGB(RGBA) images are supported.")
+except Exception as e:
+  print(e)
